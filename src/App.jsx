0f61766cb90ce0980b0ebe4c0e81c2c896cef505
unknown <aloneff220@gmail.com>
2025-11-17 10:00:59 +0530

    dist

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
-    <Router basename="/hospital-frontend/">
+    <Router basename="/cicdend/">
       <Navbar />
       <Routes>
         <Route path="/" element={<Login />} />
